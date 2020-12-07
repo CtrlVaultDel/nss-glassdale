1518208af4856d46f6ef64b5b25a720bcca79d52
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-12-07 14:13:17 -0600

    Refactored notes to automatically generate the date, instead of using an input

--- scripts/notes/noteForm.js
+++ scripts/notes/noteForm.js
@@ -9,9 +9,9 @@ const eventHub = document.querySelector(".container");
 const render = () => {
     contentTarget.innerHTML = `
         <div class="noteForm">
-            <input type="text" id="note--text" placeholder="Please enter notes here">
+            <textarea type="text" id="note--text" placeholder="Please enter notes here"></textarea>
+            <input type="text" id="note--author" placeholder="Author Name">
             <input type="text" id="note--suspect" placeholder="Suspect Name">
-            <input type="date" id="note--date">
             <button id="saveNote">Save Note</button>
             <div class="note--display">Display Notes: 
                 <input type="checkbox" checked="true" id="note--checkbox">
@@ -30,18 +30,16 @@ eventHub.addEventListener("click", clickEvent => {
 
         // ISSUE IS HERE
         let text = document.getElementById("note--text").value;
+        let author = document.getElementById("note--author").value;
         let suspect = document.getElementById("note--suspect").value;
-        let date = document.getElementById("note--date").value;
-
-        console.log(text)
-        console.log(suspect)
-        console.log(date)
+        let timestamp = new Date();
 
         // Make a new object representation of a note
         const newNote = {
             text: text,
+            author: author,
             suspect: suspect,
-            date: date
+            timestamp: timestamp
         };
 
         // Change API state and application state
