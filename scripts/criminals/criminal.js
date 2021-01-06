2094915020182fac11a8b10d62e633b50e62bd52
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-01-06 11:10:08 -0600

    Facilities are now pulled down for each criminal. Need to fix debug.log error

--- scripts/criminals/criminal.js
+++ scripts/criminals/criminal.js
@@ -1,5 +1,5 @@
 // Converts the relevant criminal object information into HTML to be used in criminalList.js
-export const criminalHTMLer = (criminalObject) =>{
+export const criminalHTMLer = (criminalObject, facilities) =>{
     return `
     <section class="criminal">
         <h3 class="criminal__name">${criminalObject.name}</h3>
@@ -19,6 +19,12 @@ export const criminalHTMLer = (criminalObject) =>{
         </div>
         <div class="criminal__incarceration-end">
             <b>Term end:</b> ${new Date(criminalObject.incarceration.end).toLocaleDateString('en-US')}
+        </div>
+        <div class = "criminal__facilities">
+            <h4 class="criminal__facilities__header">---  Facilities  ---</h4>
+            <ul>
+                ${facilities.map(f => `<li>${f.facilityName}</li>`).join("")}
+            </ul>
         </div>
     </section>
     `;
