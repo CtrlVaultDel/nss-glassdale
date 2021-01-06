f7655dfdf3d11783ca283eaec96c2a3a5c8e5cfa
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-01-06 13:57:07 -0600

    Can switch to Facilities page, just need to add content

--- scripts/switchPage.js
+++ scripts/switchPage.js
@@ -4,33 +4,58 @@ const topFilters = document.querySelector(".mainFilters");
 const alibiListing = document.querySelector(".alibisContainer");
 const criminalListing = document.querySelector(".criminalsContainer");
 const witnessListing = document.querySelector(".witnessesContainer");
+const facilityListing = document.querySelector(".facilityContainer");
 const seeWitnessesBtn = document.getElementById("seeWitnesses-btn");
 const seeCriminalsBtn = document.getElementById("seeCriminals-btn");
+const seeFacilitiesBtn = document.getElementById("seeFacilities-btn");
 
 // This is responsible for switching between states:
 // 1) Witnesses & Notes
 // 2) Criminals & Notes
 eventHub.addEventListener("click", event => {
-    if(event.target.id === "seeWitnesses-btn"){
-        // Hide these elements
-        topFilters.classList.add("hide");
-        alibiListing.classList.add("hide");
-        seeWitnessesBtn.classList.add("hide");
-        criminalListing.classList.add("hide");
 
+    // See Witnesses Page
+    if(event.target.id === "seeWitnesses-btn"){
         // Show these elements
         seeCriminalsBtn.classList.remove("hide");
+        seeFacilitiesBtn.classList.remove("hide");
         witnessListing.classList.remove("hide");
+        
+        // Hide these elements
+        seeWitnessesBtn.classList.add("hide");
+        topFilters.classList.add("hide");
+        alibiListing.classList.add("hide");
+        criminalListing.classList.add("hide");
+        facilityListing.classList.add("hide");
     }
+
+    // See Criminals Page (Default)
     else if (event.target.id === "seeCriminals-btn"){
         // Show these elements
         topFilters.classList.remove("hide");
         alibiListing.classList.remove("hide");
         seeWitnessesBtn.classList.remove("hide");
         criminalListing.classList.remove("hide");
+        seeFacilitiesBtn.classList.remove("hide");
 
         // Hide these elements
         seeCriminalsBtn.classList.add("hide");
         witnessListing.classList.add("hide");
+        facilityListing.classList.add("hide");
     }
-})
+
+    // See Facilities Page
+    else if (event.target.id === "seeFacilities-btn"){
+        // Show these elements
+        seeCriminalsBtn.classList.remove("hide");
+        seeWitnessesBtn.classList.remove("hide");
+        facilityListing.classList.remove("hide");
+
+        // Hide these elements
+        topFilters.classList.add("hide");
+        alibiListing.classList.add("hide");
+        seeFacilitiesBtn.classList.add("hide");
+        witnessListing.classList.add("hide");
+        criminalListing.classList.add("hide");
+    }
+});
